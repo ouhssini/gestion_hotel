7c1ce352bd7215391ff0979da5f22504a11379b6
Ouhssini Ahmed <ahmedouhssini@gmail.com>
2025-04-21 00:50:04 +0100

    fix home page route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+    Route::get('/', DashboardController::class)->name('dashboard');
 
     Route::resource('chambres', ChambreController::class);
     Route::resource('types', TypeController::class)->except('show');
